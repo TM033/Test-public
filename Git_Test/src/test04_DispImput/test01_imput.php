70bce428720177ced75d8558c973e1b552335090
niraniranira <nirapasta@gmail.com>
2023-11-11 19:17:45 +0900

    test04

--- Git_Test/src/test04_DispImput/test01_imput.php
+++ Git_Test/src/test04_DispImput/test01_imput.php
@@ -1,4 +1,25 @@
 <?php
+$pageToDisplay = 0; //表示すべきページ
+$kisetsu = -1;  //どこも指していない状態
+$nickname = "";
+$seibetsu = -1; //どこも指していない状態
+$kisetsu = -1;  //どこも指していない状態
+$checkmessage = "";
+
+if (isset($_POST['okuru']) == true) {
+    $pageToDisplay = 1;
+    if ($_POST['nickname'] != "") {
+        $nickname = htmlspecialchars($_POST['nickname'], ENT_QUOTES);
+    } else {
+        $pageToDisplay = 0;
+        $checkmessage .= "ニックネームが記入されていません。<br>";
+    }
+    if ($checkmessage != "") {
+        $checkmessage = "<font color=\"red\"><b>" . $checkmessage;
+        $checkmessage .= "正しく記入してもう一度送信してください。</b></font><br>& lt;br>";
+    }
+}
+
 $imput = "imput_msg";
 
 $echo_language = "python ";
@@ -11,3 +32,20 @@ exec($full_cmmand, $output);
 for ($i = 0; $i < count($output); $i++) {
     print $output[$i] . "<br>\n";
 }
+?>
+
+<html>
+
+<body>
+
+    <?php
+    print "あなたのお名前は？<br>\n";
+    ?>
+    <form action="testname2.php" method="post">
+        <input type="text" size="30" name="username"><br>
+        <input type="submit" name="soshin" value="送信">
+    </form>
+
+</body>
+
+</html>
--- Git_Test/src/test04_DispImput/test01_imput.py
+++ Git_Test/src/test04_DispImput/test01_imput.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import Util
 
@@ -11,3 +13,10 @@ if len(argvs) >= 2:
     print("Hello", imput, " getGraduationYear(1):", graduation_year)
 else:
     print("Hello")
+
+
+# PHP側でSubmitした引数によって、処理を変更する
+# 引数が1の場合、初期表示
+# 引数が2の場合（送信押下時）、入力したデータを基に画面表示を行う
+
+# まずはpython側でfront処理を記載出来るようにする
